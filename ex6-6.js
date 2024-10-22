55ec9fece6cefc70eb78a1fbdd223e6a192de343
Ryomiyamo <167832767+Ryomiyamo@users.noreply.github.com>
2024-10-22 10:05:43 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,6 +1,6 @@
 var who66='world';function sayhello66(){alert('Heloo,'+who66+'!');}
 function taro66(){who66='太郎';}
 function hanako66(){who66='花子';}
-function taro66(){alert('Hello,Funtion!');}
-function hanako66(){alert("Goodbye!");}
+function taro66(){alert('太郎が来た');}
+function hanako66(){alert("花子が来た");}
 
