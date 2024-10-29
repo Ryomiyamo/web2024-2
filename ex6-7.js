e64d30bb3b72f3a9995ab749d4db9d6c61501961
Ryomiyamo <167832767+Ryomiyamo@users.noreply.github.com>
2024-10-29 09:10:54 +0900

    Update ex6-7.js

--- ex6-7.js
+++ ex6-7.js
@@ -1,2 +1,3 @@
-var who67='world;function sayhello67(){alert('Hello<'+who67+'!');}
-funxtion someone(x){who67=x;}
+var who67='world;
+function sayhello67(){alert('Hello,'+who67+'!');}
+function someone(x){who67=x;}
